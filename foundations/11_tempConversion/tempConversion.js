8c73ce042d5bb116de34617f622445fe3e0d5ebc
Alex Ha <haalex1101@gmail.com>
2026-01-06 23:22:52 -0500

    Finish exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const tempC = (tempF - 32) * 5 / 9;
+  const roundFactor = Math.pow(10, 1);
+  return Math.round(tempC * roundFactor) / roundFactor;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  const tempF = (tempC * 1.8) + 32;
+  const roundFactor = Math.pow(10, 1);
+  return Math.round(tempF * roundFactor) / roundFactor;
 };
 
 // Do not edit below this line
