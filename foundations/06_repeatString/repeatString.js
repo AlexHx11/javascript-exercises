fa05c067efd0526e3eac5f3e7d56bba8d166eda7
Alex Ha <haalex1101@gmail.com>
2026-01-06 22:02:22 -0500

    Finish exercise 6-7
    Exercise reverseString and repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(val, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let str = "";
+    for (let i = 0; i < num; i++) {
+        str = str + val;
+    }
+    return str;
 };
 
 // Do not edit below this line
