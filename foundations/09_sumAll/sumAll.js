8d3cddfa22768a8ed9ad051c8ad11cede0542554
Alex Ha <haalex1101@gmail.com>
2026-01-06 23:01:32 -0500

    Finish exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof(a) != "number" || typeof(b) != "number" || a < 0 || b < 0 || a % 1 != 0 || b % 1 != 0) {
+        return "ERROR";
+    }
+    
+    let start, stop;
+    if (a > b) {
+        stop = a;
+        start = b;
+    } else {
+        stop = b;
+        start = a;
+    }
 
+    return Array
+        .from({length: (stop-start)+1}, (val, i) => start + i)
+        .reduce((sum, curr) => sum + curr, 0);
 };
 
 // Do not edit below this line
